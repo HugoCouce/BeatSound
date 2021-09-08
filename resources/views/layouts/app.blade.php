7a0c829aa4d8c1d843f17a30c86d45909fe66a81
HugoCouce <hugocoucelorenzo@gmail.com>
2021-09-08 11:59:58 +0200

    Ajustes de barra de navegación

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,76 +22,78 @@
 
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="/"><img src="{{URL::asset('/images/BeatSoundLogo.png')}}" alt="logo"></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top" id="mainNav">
+                <a class="navbar-brand" href="/"><img src="{{URL::asset('/images/BeatSoundLogo.png')}}" alt="logo"></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="/">Inicio</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Productos
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ url('/product/buy') }}">Todos los productos</a>
-                            <a class="dropdown-item" href="{{ url('/product/buy/vinyl') }}">Vinilos</a>
-                            <a class="dropdown-item" href="{{ url('/product/buy/cd') }}">CD´s</a>
-                        </div>
-                    </li>
-                </ul>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav text-uppercase ms-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="/">Inicio</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Productos
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ url('/product/buy') }}">Todos los productos</a>
+                                <a class="dropdown-item" href="{{ url('/product/buy/vinyl') }}">Vinilos</a>
+                                <a class="dropdown-item" href="{{ url('/product/buy/cd') }}">CD´s</a>
+                            </div>
+                        </li>
+                    </ul>
 
-                <ul class="navbar-nav ml-auto">
-                    <!-- Authentication Links -->
-                    @guest
-                    @if (Route::has('login'))
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">{{ __('Acceso') }}</a>
-                    </li>
-                    @endif
+                    <ul class="navbar-nav text-uppercase ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                        @if (Route::has('login'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Acceso') }}</a>
+                        </li>
+                        @endif
 
-                    @if (Route::has('register'))
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('register') }}">{{ __('Registro') }}</a>
-                    </li>
-                    @endif
-                    @else
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->nombre }}
-                        </a>
-
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-
-
-                            @if (Auth::user()->administrador==1)
-                            <a class="dropdown-item" href="{{ url('/user') }}">
-                                {{ __('Editar cuentas') }}
+                        @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Registro') }}</a>
+                        </li>
+                        @endif
+                        @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->nombre }}
                             </a>
 
-                            @else
-                            <a class="dropdown-item" href="{{ url('/user/'.Auth::user()->user_dni.'/edit') }}">
-                                {{ __('Editar datos') }}
-                            </a>
-                            @endif
-                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+
+
+                                @if (Auth::user()->administrador==1)
+                                <a class="dropdown-item" href="{{ url('/user') }}">
+                                    {{ __('Editar cuentas') }}
+                                </a>
+
+                                @else
+                                <a class="dropdown-item" href="{{ url('/user/'.Auth::user()->user_dni.'/edit') }}">
+                                    {{ __('Editar datos') }}
+                                </a>
+                                @endif
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                {{ __('Cerrar sesión') }}
-                            </a>
+                                    {{ __('Cerrar sesión') }}
+                                </a>
 
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
-                        </div>
-                    </li>
-                    @endguest
-                </ul>
-            </div>
-        </nav>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                        @endguest
+                    </ul>
+                </div>
+            </nav>
+        </div>
 
         <main class="py-4">
             @yield('content')
